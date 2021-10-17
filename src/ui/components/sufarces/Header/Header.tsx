30b9c5e4c42eb73e34c318545babc1d3c2032e85
douglasgvr <douglasfarmaangra@gmail.com>
2021-10-17 18:48:44 -0300

    arrumando o codigo que estava dando erro

--- src/ui/components/sufarces/Header/Header.tsx
+++ src/ui/components/sufarces/Header/Header.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { HeaderAppBar, HeaderLogo } from "./Header.style";
-import { Toolbar } from "@material-ui/core";
+import { Toolbar, Container } from "@material-ui/core";
 
 const Header: React.FC = () => {
   return (
     <HeaderAppBar position={"sticky"}>
       <Toolbar>
-        <HeaderLogo src={"/img/logos/logo-icon.png"} alt={"e-diaristas"} />
+        <HeaderLogo src={"/img/logos/logo.svg"} alt={"e-diaristas"} />
       </Toolbar>
     </HeaderAppBar>
   );
