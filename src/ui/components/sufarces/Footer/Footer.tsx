72f7d26ab7b1b964d557625ca71b39b92847b67c
douglasgvr <douglasfarmaangra@gmail.com>
2021-10-25 22:52:42 -0300

    Footer, Flexbox.

--- src/ui/components/sufarces/Footer/Footer.tsx
+++ src/ui/components/sufarces/Footer/Footer.tsx
@@ -1,20 +1,40 @@
 import React from "react";
-import { FooterStyled, FooterContainer, FooterTitle } from "./Footer.style";
-import { Typography } from "@material-ui/core";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@material-ui/core";
 
 export const Footer = () => {
   return (
     <FooterStyled>
       <FooterContainer>
-        Douglas
-        <FooterTitle>Quem Somos</FooterTitle>
-        <Typography>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
-          dolore tenetur natus sed quia? Laudantium velit, repellat quos atque,
-          cumque itaque similique, est error soluta exercitationem nesciunt.
-          Iure, reprehenderit vitae?
-        </Typography>
-        <FooterTitle>Quem Somos</FooterTitle>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Quem Somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            E-Diaristas te ajuda a encontrar um profissional perfeito para
+            realizar a limpeza da sua casa. Garantimos o(a) melhor profissional
+            com total segurança e praticidade! São milhares de clientes
+            satisfeitos por todo o país.
+          </Typography>
+        </Box>
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App Store"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google Play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
       </FooterContainer>
     </FooterStyled>
   );
